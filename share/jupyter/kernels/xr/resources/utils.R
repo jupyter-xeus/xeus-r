7be30fa84192dcce3f7904d884705614c3d2a6a4
Romain Francois <romain@purrple.cat>
2023-10-26 15:19:42 +0200

    borrow IRkernel::plot_builds_upon

--- share/jupyter/kernels/xr/resources/utils.R
+++ share/jupyter/kernels/xr/resources/utils.R
@@ -5,3 +5,15 @@ get_os <- function() {
         unix = if (identical(Sys.info()[['sysname']], 'Darwin')) 'osx' else 'unix'
     )
 }
+
+# borrowed from IRkernel
+plot_builds_upon <- function(prev, current) {
+    if (is.null(prev)) {
+        return(TRUE)
+    }
+    
+    lprev <- length(prev[[1]])
+    lcurrent <- length(current[[1]])
+    
+    lcurrent >= lprev && identical(current[[1]][1:lprev], prev[[1]][1:lprev])
+}
