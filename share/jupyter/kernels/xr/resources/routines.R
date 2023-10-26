f88fc078c55776b2efcea91bb27e6bb8f4f0a0b5
Romain Francois <romain@purrple.cat>
2023-10-26 15:20:17 +0200

    rm invisible()

--- share/jupyter/kernels/xr/resources/routines.R
+++ share/jupyter/kernels/xr/resources/routines.R
@@ -1,16 +1,16 @@
 
 publish_execution_error <- function(ename, evalue, trace_back = character()) {
-  invisible(.Call("xeusr_publish_execution_error", ename, evalue, trace_back))
+  .Call("xeusr_publish_execution_error", ename, evalue, trace_back, PACKAGE = "(embedding)")
 }
 
 publish_execution_result <- function(execution_count, data, metadata = NULL) {
-  invisible(.Call("xeusr_publish_execution_result", as.integer(execution_count), jsonlite::toJSON(data), jsonlite::toJSON(metadata)))
+  .Call("xeusr_publish_execution_result", as.integer(execution_count), jsonlite::toJSON(data), jsonlite::toJSON(metadata), PACKAGE = "(embedding)")
 }
 
 publish_stream <- function(name, text) {
-  invisible(.Call("xeusr_publish_stream", name, text, PACKAGE = "(embedding)"))
+  .Call("xeusr_publish_stream", name, text, PACKAGE = "(embedding)")
 }
 
 display_data <- function(data = NULL, metadata = NULL) {
-  .Call("xeusr_display_data", jsonlite::toJSON(data), jsonlite::toJSON(metadata))
+  .Call("xeusr_display_data", jsonlite::toJSON(data), jsonlite::toJSON(metadata), PACKAGE = "(embedding)")
 }
