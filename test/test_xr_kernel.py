8691c619120d6154b08990e268730317478c2be4
Romain Francois <romain@purrple.cat>
2023-11-07 09:55:13 +0100

    uncommenting test

--- test/test_xr_kernel.py
+++ test/test_xr_kernel.py
@@ -6,20 +6,14 @@
 # The full license is in the file LICENSE, distributed with this software.
 #############################################################################
 
+import tempfile
+import unittest
+import jupyter_kernel_test
 
-#########################################################################################
-#                            TO UNCOMMENT AND ADAPT                                     #
-#########################################################################################
+class KernelTests(jupyter_kernel_test.KernelTests):
 
-#import tempfile
-#import unittest
-#import jupyter_kernel_test
-
-
-#class KernelTests(jupyter_kernel_test.KernelTests):
-
-    #kernel_name = "xr"
-    #language_name = "R"
+    kernel_name = "xr"
+    language_name = "R"
     #code_hello_world = "hello, world"
     #code_page_something = "?"
     #completion_samples = [{"text": "H", "matches": {"Hello", "Hey", "Howdy"}}]
@@ -28,12 +22,12 @@
     #invalid_code_samples = ["invalid"]
     #code_inspect_sample = "print"
 
-    #def test_stdout(self):
-        #self.flush_channels()
-        #reply, output_msgs = self.execute_helper(code="hello, world")
-        #self.assertEqual(output_msgs[0]["msg_type"], "stream")
-        #self.assertEqual(output_msgs[0]["content"]["name"], "stdout")
-        #self.assertEqual(output_msgs[0]["content"]["text"], "hello, world")
+    def test_stdout(self):
+        self.flush_channels()
+        reply, output_msgs = self.execute_helper(code="cat('hello, world')")
+        self.assertEqual(output_msgs[0]["msg_type"], "stream")
+        self.assertEqual(output_msgs[0]["content"]["name"], "stdout")
+        self.assertEqual(output_msgs[0]["content"]["text"], "hello, world")
 
     #def test_stderr(self):
         #self.flush_channels()
