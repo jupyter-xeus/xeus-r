85606d73924f8ceea3a1b7dfa5e6131a305fc210
Romain François <romain@purrple.cat>
2023-11-09 10:27:02 +0100

    Test (#23)
    * rm fake test
    
    * ignore __pycache__
    
    * uncommenting test
    
    * adjust tests
    
    * set RHOME
    
    * skip pager test for now
    
    * also skip execute_result for now
    
    * borrow def _execute_code from IRkernel
    
    * set R_HOME

--- test/test_xr_kernel.py
+++ test/test_xr_kernel.py
@@ -6,55 +6,48 @@
 # The full license is in the file LICENSE, distributed with this software.
 #############################################################################
 
-
-##########################################################################################
-# TODO
-# Please remove fake test and uncomment the code lines underneath it in order to enable
-# the actual tests (to be modified/adapted)
-#########################################################################################
-# These tests examples (to be modified) check that the given code "hello, world" would be
-# published as text stdout stream message and "error" as stderr
-# Other tests are performed during the initialization of `jupyter_kernel_test.KernelTests`
-# and inside the `execute_helper` function based on the given variables defined within
-# the class
-#########################################################################################
-#                               TO BE REMOVED                                           #
-#########################################################################################
-
+import tempfile
 import unittest
+import jupyter_kernel_test
 
-class TestFake(unittest.TestCase):
+class KernelTests(jupyter_kernel_test.KernelTests):
 
-    def test_fake(self):
-        pass
+    kernel_name = "xr"
+    language_name = "R"
 
-#########################################################################################
-#                            TO UNCOMMENT AND ADAPT                                     #
-#########################################################################################
+    def _execute_code(self, code, tests=True, silent=False, store_history=True):
+        self.flush_channels()
 
-#import tempfile
-#import unittest
-#import jupyter_kernel_test
+        reply, output_msgs = self.execute_helper(code, silent=silent, store_history=store_history)
 
+        self.assertEqual(reply['content']['status'], 'ok', '{0}: {0}'.format(reply['content'].get('ename'), reply['content'].get('evalue')))
+        if tests:
+            self.assertGreaterEqual(len(output_msgs), 1)
+            # xeusr does the same as irkernel: only sends display_data, not execute_result
+            self.assertEqual(output_msgs[0]['msg_type'], 'display_data')
+        return reply, output_msgs
 
-#class KernelTests(jupyter_kernel_test.KernelTests):
-
-    #kernel_name = "xr"
-    #language_name = "R"
-    #code_hello_world = "hello, world"
-    #code_page_something = "?"
-    #completion_samples = [{"text": "H", "matches": {"Hello", "Hey", "Howdy"}}]
+    code_hello_world = "cat('hello, world')"
+    # code_page_something = "?cat"
+    completion_samples = [{"text": "H", "matches": {"Hello", "Hey", "Howdy"}}]
     #complete_code_samples = ["hello, world"]
+    code_execute_result = [{"code": "6*7", "result": "[1] 42"}]
     #incomplete_code_samples = ["incomplete"]
     #invalid_code_samples = ["invalid"]
     #code_inspect_sample = "print"
 
-    #def test_stdout(self):
-        #self.flush_channels()
-        #reply, output_msgs = self.execute_helper(code="hello, world")
-        #self.assertEqual(output_msgs[0]["msg_type"], "stream")
-        #self.assertEqual(output_msgs[0]["content"]["name"], "stdout")
-        #self.assertEqual(output_msgs[0]["content"]["text"], "hello, world")
+    def test_stdout(self):
+        self.flush_channels()
+        reply, output_msgs = self.execute_helper(code="cat('hello, world')")
+        self.assertEqual(output_msgs[0]["msg_type"], "stream")
+        self.assertEqual(output_msgs[0]["content"]["name"], "stdout")
+        self.assertEqual(output_msgs[0]["content"]["text"], "hello, world")
+
+    def test_execute_result(self):
+        self.flush_channels()
+        reply, output_msgs = self._execute_code(code="6*7")
+        data = output_msgs[0]['content']['data']
+        self.assertEqual(data['text/plain'], ['[1] 42'])
 
     #def test_stderr(self):
         #self.flush_channels()
