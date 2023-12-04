5a04322b5e12b5dc187f5f03842dfcbe20780dce
Romain François <romain@purrple.cat>
2023-12-04 16:26:03 +0100

    handle html widgets (#55)
    * update demo notebook
    
    * htmlwidgets get text/html mime type as well automatically for auto print
    
    * minimal test for shiny.tag

--- share/jupyter/kernels/xr/resources/execute.R
+++ share/jupyter/kernels/xr/resources/execute.R
@@ -167,10 +167,14 @@ execute <- function(code, execution_counter, silent = FALSE) {
   if (isTRUE(last_visible)) {
     obj <- .Last.value
 
-    # TODO: some objets should be auto promoted to a different mime bundle
-    #       e.g. html widgets
-
-    bundle <- IRdisplay::prepare_mimebundle(obj, mimetypes = "text/plain")
+    # TODO: This probably needs to be generalized
+    mimetypes <- if (inherits(obj, c("htmlwidget", "shiny.tag.list", "shiny.tag"))) {
+      c("text/plain", "text/html")
+    } else {
+      "text/plain"
+    }
+    
+    bundle <- IRdisplay::prepare_mimebundle(obj, mimetypes = mimetypes)
     publish_execution_result(execution_counter, bundle$data, bundle$metadata)
   }
 
