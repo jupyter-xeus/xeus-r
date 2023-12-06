c758b4c10668c4f6f5535b14fd467b3eb00aa488
Romain Francois <romain@purrple.cat>
2023-12-06 11:18:25 +0100

    use text/html

--- share/jupyter/kernels/xr/resources/inspect.R
+++ share/jupyter/kernels/xr/resources/inspect.R
@@ -2,7 +2,7 @@ inspect <- function(code, cursor_pos) {
     txt <- glue::glue("hola {Sys.time()}")
     data <- jsonlite::toJSON(list(
         'text/plain' = jsonlite::unbox(txt), 
-        'text/html' = jsonlite::unbox(glue::glue("<h2>{txt}</h2>"))
+        'text/html' = jsonlite::unbox(glue::glue("<h2>hola</h2><p>{txt}</p>"))
     ))
     metadata <- NULL
     list(found = TRUE, data = data, metadata = metadata)
