42112745c7ae501494f36556ff6cc3032205db3b
Romain Francois <romain@tada.science>
2024-05-13 16:32:42 +0200

    inform that hera is not loaded from xeusr

--- hera/R/zzz.R
+++ hera/R/zzz.R
@@ -8,4 +8,14 @@ utils <- NULL
   utils <<- asNamespace("utils")
   init_options()
   init_overwritten()
+
+  dlls <- getLoadedDLLs()
+  if (!"(embedding)" %in% names(dlls)) {
+    packageStartupMessage("hera is meant to be loaded from within xeusr")
+  } else {
+    dot_call <- names(getDLLRegisteredRoutines("(embedding)")$.Call)
+    if (! "xeusr_kernel_info_request" %in% dot_call) {
+      packageStartupMessage("hera is meant to be loaded from within xeusr")
+    }
+  }
 }
