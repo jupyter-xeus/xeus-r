51c36207b119cad029d75f0f72a33fba1a54d405
anutosh491 <andersonbhat491@gmail.com>
2024-07-10 16:02:10 +0530

    Fix Docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,9 +16,9 @@ html_theme = "sphinx_rtd_theme"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 def setup(app):
-    app.add_stylesheet("main_stylesheet.css")
+    app.add_css_file("main_stylesheet.css")
 
-extensions = ['breathe']
+extensions = ['breathe', "sphinx_rtd_theme"]
 breathe_projects = { 'xeus-r': '../xml' }
 templates_path = ['_templates']
 html_static_path = ['_static']
