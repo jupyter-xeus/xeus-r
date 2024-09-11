9dcb45a35b04a0bffb160d39316025c10b4eb350
Romain Francois <romain@tada.science>
2024-09-11 13:34:06 +0200

    use the ...

--- share/jupyter/kernels/xr/resources/call.R
+++ share/jupyter/kernels/xr/resources/call.R
@@ -2,6 +2,6 @@
     get(fn, envir = .xeus_env)(...)
 }
 
-.xeus_new <- function(class, xp) {
-    get(class, envir = .xeus_env)$new(xp)
+.xeus_new <- function(class, xp, ...) {
+    get(class, envir = .xeus_env)$new(xp, ...)
 }
