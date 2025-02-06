993ea51e87a4ba592ee2d77a55ffffbfe86a71c4
Romain Francois <romain@tada.science>
2025-02-06 13:38:14 +0100

    + hera_dot_call()

--- hera/R/zzz.R
+++ hera/R/zzz.R
@@ -12,3 +12,23 @@ hera_call <- function(fn, ...) {
 hera_new <- function(class, xp) {
     get(class, envir = NAMESPACE)$new(xp)
 }
+
+is_xeus <- function() {
+  dlls <- getLoadedDLLs()
+  embeding <- dlls[["(embedding)"]]
+  !is.null(embedding) && "xeusr_kernel_info_request" %in% getDLLRegisteredRoutines()$.Call
+}
+
+hera_dot_call <- function(fn, ...) {
+  # TODO: check that we are indeed withing xeus-r
+  # and have some sort of plan B
+
+  # if (!is_xeusr()) {
+  #   ...
+  # }
+
+  call <- rlang::call2(".Call", fn, ..., PACKAGE = "(embedding)")
+  eval.parent(call)
+}
+
+
