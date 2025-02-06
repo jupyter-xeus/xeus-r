e3512484094e8e59ab248d54bb85060035f5d8d2
Romain Francois <romain@tada.science>
2025-02-06 13:38:46 +0100

    move completion code to hera

---
+++ hera/R/completion.R
@@ -0,0 +1,42 @@
+triple_colon <- function(pkg, fun) {
+  eval(rlang::call2(":::", as.symbol(pkg), as.symbol(fun)))
+}
+
+utils___assignLineBuffer    <- triple_colon("utils", ".assignLinebuffer")
+utils___assignEnd           <- triple_colon("utils", ".assignLinebuffer")
+utils___guessTokenFromLine  <- triple_colon("utils", ".guessTokenFromLine")
+utils___completeToken       <- triple_colon("utils", ".completeToken")
+utils___retrieveCompletions <- triple_colon("utils", ".retrieveCompletions")
+
+# This is mostly inspired from IRkernel::completions()
+complete <- function(code, cursor_pos = nchar(code)) {
+    # Find which line we're on and position within that line
+    lines <- strsplit(code, '\n', fixed = TRUE)[[1]]
+    chars_before_line <- 0L
+    for (line in lines) {
+        new_cursor_pos <- cursor_pos - nchar(line) - 1L # -1 for the newline
+        if (new_cursor_pos < 0L) {
+            break
+        }
+        cursor_pos <- new_cursor_pos
+        chars_before_line <- chars_before_line + nchar(line) + 1L
+    }
+
+    # guard from errors when completion is invoked in empty cells
+    if (is.null(line)) {
+        line <- ''
+    }
+
+    utils___assignLineBuffer(line)
+    utils___assignEnd(cursor_pos)
+
+    info <- utils___guessTokenFromLine(update = FALSE)
+    utils___guessTokenFromLine()
+    utils___completeToken()
+
+    start_position <- chars_before_line + info$start
+    comps <- utils___retrieveCompletions()
+
+    # TODO: use jsonlite::toJSON() here
+    list(comps, c(start_position, start_position + nchar(info$token)))
+}
